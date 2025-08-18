81bd6f98ef98c9c6d330e2855f58b8cddec8f6a9
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-18 11:57:58 -0300

    feat: adicionando funcionalidades de filtro pela query na busca por cursos

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
+import { asc, ilike } from "drizzle-orm";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,6 +11,12 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["courses"],
         summary: "Get all courses",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["id", "title"]).optional().default("id"),
+          page: z.coerce.number().optional().default(1),
+          limit: z.coerce.number().optional().default(2),
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -18,14 +25,27 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 title: z.string(),
               })
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db.select().from(courses);
+      const { search, orderBy, page, limit } = request.query;
+      const where = search ? ilike(courses.title, `%${search}%`) : undefined;
 
-      reply.send({ courses: result });
+      const [result, total] = await Promise.all([
+        db
+          .select()
+          .from(courses)
+          .orderBy(asc(courses[orderBy]))
+          .limit(limit)
+          .offset((page - 1) * limit)
+          .where(where),
+        db.$count(courses, where),
+      ]);
+
+      reply.send({ courses: result, total });
     }
   );
 };
