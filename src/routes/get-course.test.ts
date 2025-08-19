4c7ebff43a227c654c9f91cb2fb509f297759f9d
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-19 17:10:43 -0300

    feat: adicionando mais testes e2e

--- src/routes/get-course.test.ts
+++ src/routes/get-course.test.ts
@@ -1,22 +1,31 @@
 import { expect, test } from "vitest";
 import request from "supertest";
 import { server } from "../app";
-import { faker } from "@faker-js/faker";
 import { makeCourse } from "../tests/factories/make-course";
+import { randomUUID } from "node:crypto";
 
 test("Get course by id", async () => {
   await server.ready();
 
-  const course = await makeCourse();
+  const fakeTitle = randomUUID();
 
-  const response = await request(server.server).get(`/courses/${course.id}`);
+  const course = await makeCourse(fakeTitle);
+
+  const response = await request(server.server).get(
+    `/courses?search=${fakeTitle}`
+  );
 
   console.log(response.body);
 
   expect(response.status).toBe(200);
-  expect(response.body.course).toEqual({
-    id: expect.any(String),
-    title: expect.any(String),
-    description: null,
+  expect(response.body).toEqual({
+    total: 1,
+    courses: [
+      {
+        id: expect.any(String),
+        title: fakeTitle,
+        enrollments: 0,
+      },
+    ],
   });
 });
