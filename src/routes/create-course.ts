ecabbd809df03be7bff620d04eab1eeb40701221
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-19 23:15:53 -0300

    feat: adicionando funcionalidade de checar a role do usuário

--- src/routes/create-course.ts
+++ src/routes/create-course.ts
@@ -2,11 +2,14 @@ import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import { z } from "zod";
+import { checkRequestJwt } from "./hooks/check-request-jwt.ts";
+import { checkUserRole } from "./hooks/check-user-role.ts";
 
 export const createCourseRoute: FastifyPluginAsyncZod = async (server) => {
   server.post(
     "/courses",
     {
+      preHandler: [checkRequestJwt, checkUserRole("manager")],
       schema: {
         body: z.object({
           title: z
---
+++ src/routes/hooks/check-user-role.ts
@@ -0,0 +1,10 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { getAuthenticatedUserFromRequest } from "../../utils/get-authenticated-user-from-request";
+
+export function checkUserRole(role: "student" | "manager") {
+  return async (request: FastifyRequest, reply: FastifyReply) => {
+    const user = getAuthenticatedUserFromRequest(request);
+
+    if (user.role !== role) return reply.status(401).send();
+  };
+}
