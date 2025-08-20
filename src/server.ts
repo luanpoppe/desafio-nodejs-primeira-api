9bca7b9fe000cc41015db03c363eaff786954b24
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-20 01:10:26 -0300

    feat: criando Dockerfile

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { server } from "./app";
 
-server.listen({ port: 3333 }).then(() => {
+server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
